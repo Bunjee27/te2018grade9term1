8416cb8f42bd972e8f1b8faaf8a1871eccee42f3
Amadou Gamby <agamby23@gmail.com>
2018-10-06 15:45:45 -0400

    Completed Challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,25 @@
 // Example
+const READLINE = require('readline-sync');
 let message = "Hello World!";
 console.log(message);
-
-// Type your code below!
+let july = 727;
+console.log(july);
+let digits = 7584;
+console.log(digits);
+let school = 101
+console.log(school);
+let patriots = "football";
+console.log(patriots);
+let b3 = "laptop";
+console.log(b3);
+let bolt = "track and field";
+console.log(bolt);
+let nba = "cavaliers";
+nba = false;
+console.log(nba)
+let bos = "celtics"
+bos = true
+console.log(bos)
+let userinput = READLINE.question('i am the greatest!')
+console.log(`You typed: ${userinput}`);
+//  
