9137832359beaf62cf30effa11d075a345941f5f
Amadou Gamby <agamby23@gmail.com>
2018-10-20 12:43:08 -0400

    Finished objects

---
+++ objects.js
@@ -0,0 +1,20 @@
+let villain;
+let hacker;
+villain={
+	name: "Shellshock",
+	catchPhrase: "You got cracked!",
+	crime: "Murder"
+};
+hacker={
+	name: "Bunjee",
+	nickName: "SSSniperWolf",
+	catchPhrase: "catch me Outside",
+	archenemy: "villain"
+};
+console.log(`I'm ${villain.name}`);
+console.log(`I like ${villain.crime}`);
+console.log(`When I kill you I say ${villain.catchPhrase}`);
+console.log(`I am a hacker and my name is ${hacker.name}`);
+console.log(`You can also call me ${hacker.nickName}`);
+console.log(`My archenemy is ${villain.name}`);
+console.log(`If you want beef then you can ${hacker.catchPhrase}`);
